722abf4e7be74cd61289454dd327352ee7140255
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2025-01-16 09:34:24 +0700

    feat deploy service

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const jsonServer = require("json-server");
+const jsonServerAuth = require("json-server-auth");
+const fs = require("fs");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+
+const rules = jsonServerAuth.rewriter({
+  // Permission rules
+  users: 600,
+  products: 644,
+  carts: 600,
+});
+
+server.db = router.db;
+server.use(rules);
+server.use(jsonServerAuth);
+server.use(router);
+
+server.listen(port);
+
+// Export the Server API
+module.exports = server;
